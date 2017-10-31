41c0645ce46c4fa1561ecea38c6f0fedfdd59ed8
mi <michael.raba@gmail.com>
2017-10-30 23:34:31 -0400

    Add message here

---
+++ phy/auto/pl9.el
@@ -0,0 +1,35 @@
+(TeX-add-style-hook
+ "pl9"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("scrartcl" "paper=a4" "fontsize=11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("bclogo" "tikz") ("fontenc" "T1") ("babel" "english") ("circuitikz" "americanresistors" "siunitx")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "scrartcl"
+    "scrartcl10"
+    "siunitx"
+    "soul"
+    "bclogo"
+    "fontenc"
+    "babel"
+    "amsmath"
+    "amsfonts"
+    "amsthm"
+    "lipsum"
+    "sectsty"
+    "fancyhdr"
+    "tikz"
+    "amssymb"
+    ""
+    "latexsym"
+    "graphicx"
+    "circuitikz")
+   (TeX-add-symbols
+    '("cx" 1)
+    '("horrule" 1)
+    "ihat"
+    "jhat"))
+ :latex)
+
